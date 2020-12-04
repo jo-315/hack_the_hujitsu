ba709a404de74de1cb7dd7f28e5b45f0c65fb86e
jo-315 <32921100+jo-315@users.noreply.github.com>
2020-12-04 09:24:35 +0900

    add api

---
+++ src/static/index.js
@@ -0,0 +1,19 @@
+var fetch_step = setInterval(function(){
+  // fetch api でlocalhostと通信
+  fetch('http://localhost:5050/fetch', {
+    method: "GET"
+  })
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    // 歩数の取り出し
+    steps = response.steps
+
+    // 歩数をelementに渡す
+    document.getElementById("steps").innerHTML = steps
+  });
+
+}, 3000);
+
+// TODO: google map api と通信？？
