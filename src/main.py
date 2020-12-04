9dc8cceaa751901634655155ba91eefbc0c8d0a6
hiroya fukuda <osaka481859@yahoo.ne.jp>
2020-12-04 10:43:05 +0900

    HTML整理

--- src/main.py
+++ src/main.py
@@ -4,8 +4,10 @@ from src import app
 # 最初の検索画面
 @app.route('/', methods=['GET'])
 def index():
+    msg = "歩数を記録"
+    point={"ロンドン":"https://www.google.com/maps/embed?pb=!4v1607025863243!6m8!1m7!1sierMTh4ePdwoKu6_7IiCvA!2m2!1d51.50897647482117!2d-0.08739389902462565!3f180!4f0!5f0.7820865974627469","ラスベガス":"https://www.google.com/maps/embed?pb=!4v1607026447718!6m8!1m7!1s2OlsjeKFZu4GQN0EOi4lHA!2m2!1d36.11234006692544!2d-115.1749467008171!3f90!4f0!5f0.7820865974627469"  }
+    return render_template("index.html", msg = msg,point =  point)
 
-    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5050, debug=True)
