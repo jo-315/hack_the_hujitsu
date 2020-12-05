3a17dc831e0c33b2b00a4be44a06805dbe8bf43a
204x115x <65213163+204x115x@users.noreply.github.com>
2020-12-05 16:18:45 +0900

    角度計算

--- src/calc_latlon.py
+++ src/calc_latlon.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def get_moving(current_lat, current_lon, theta, step):
+def calc_moving(current_lat, current_lon, theta, step):
     """
     Args:
         current_lat (float): 現在地の北緯
@@ -24,6 +24,32 @@ def get_moving(current_lat, current_lon, theta, step):
     #print(next_lat, next_lon)
     return next_lat, next_lon
     
+
+def calc_theta(START, GOAL):
+    """
+    出発地, 到着地から向かうべき方角を決める
+
+    Args:
+        START: 出発地点
+            ( START = {'latitude': -157.85059440538518, 'longitude': 21.292003771901275} )
+        GOAL: 到着地点
+            ( GOAL = {'latitude': -157.84389066977633, 'longitude': 21.289743961838436} )
+    
+    Returns:
+        theta (float): 方向(0 ~ 360)
+    """
+    start_plot = trans_xy(START["latitude"], START["longitude"], 0, 0)
+    goal_plot = trans_xy(GOAL["latitude"], GOAL["longitude"], 0, 0)
+    x_dist = (goal_plot[0] - start_plot[0])
+    y_dist = goal_plot[1] - start_plot[1]
+    tan = y_dist / x_dist
+    theta = np.rad2deg(np.arctan(tan))
+
+    # 東経, 北緯による条件分岐
+    theta += 360 # 今回のスタート, ゴールの場合
+    return theta
+
+
 
 def trans_lat_lon(x, y, phi0_deg, lambda0_deg):
     """ 
@@ -109,8 +135,81 @@ def trans_lat_lon(x, y, phi0_deg, lambda0_deg):
     return np.rad2deg(latitude), np.rad2deg(longitude) # [deg]
 
 
+def trans_xy(phi_deg, lambda_deg, phi0_deg, lambda0_deg):
+    """ 緯度経度を平面直角座標に変換する
+    - input:
+        (phi_deg, lambda_deg): 変換したい緯度・経度[度]（分・秒でなく小数であることに注意）
+        (phi0_deg, lambda0_deg): 平面直角座標系原点の緯度・経度[度]（分・秒でなく小数であることに注意）
+    - output:
+        x: 変換後の平面直角座標[m]
+        y: 変換後の平面直角座標[m]
+    """
+    # 緯度経度・平面直角座標系原点をラジアンに直す
+    phi_rad = np.deg2rad(phi_deg)
+    lambda_rad = np.deg2rad(lambda_deg)
+    phi0_rad = np.deg2rad(phi0_deg)
+    lambda0_rad = np.deg2rad(lambda0_deg)
+
+    # 補助関数
+    def A_array(n):
+        A0 = 1 + (n**2)/4. + (n**4)/64.
+        A1 = -     (3./2)*( n - (n**3)/8. - (n**5)/64. ) 
+        A2 =     (15./16)*( n**2 - (n**4)/4. )
+        A3 = -   (35./48)*( n**3 - (5./16)*(n**5) )
+        A4 =   (315./512)*( n**4 )
+        A5 = -(693./1280)*( n**5 )
+        return np.array([A0, A1, A2, A3, A4, A5])
+
+    def alpha_array(n):
+        a0 = np.nan # dummy
+        a1 = (1./2)*n - (2./3)*(n**2) + (5./16)*(n**3) + (41./180)*(n**4) - (127./288)*(n**5)
+        a2 = (13./48)*(n**2) - (3./5)*(n**3) + (557./1440)*(n**4) + (281./630)*(n**5)
+        a3 = (61./240)*(n**3) - (103./140)*(n**4) + (15061./26880)*(n**5)
+        a4 = (49561./161280)*(n**4) - (179./168)*(n**5)
+        a5 = (34729./80640)*(n**5)
+        return np.array([a0, a1, a2, a3, a4, a5])
+
+    # 定数 (a, F: 世界測地系-測地基準系1980（GRS80）楕円体)
+    m0 = 0.9999 
+    a = 6378137.
+    F = 298.257222101
+
+    # (1) n, A_i, alpha_iの計算
+    n = 1. / (2*F - 1)
+    A_array = A_array(n)
+    alpha_array = alpha_array(n)
+
+    # (2), S, Aの計算
+    A_ = ( (m0*a)/(1.+n) )*A_array[0] # [m]
+    S_ = ( (m0*a)/(1.+n) )*( A_array[0]*phi0_rad + np.dot(A_array[1:], np.sin(2*phi0_rad*np.arange(1,6))) ) # [m]
+
+    # (3) lambda_c, lambda_sの計算
+    lambda_c = np.cos(lambda_rad - lambda0_rad)
+    lambda_s = np.sin(lambda_rad - lambda0_rad)
+
+    # (4) t, t_の計算
+    t = np.sinh( np.arctanh(np.sin(phi_rad)) - ((2*np.sqrt(n)) / (1+n))*np.arctanh(((2*np.sqrt(n)) / (1+n)) * np.sin(phi_rad)) )
+    t_ = np.sqrt(1 + t*t)
+
+    # (5) xi', eta'の計算
+    xi2  = np.arctan(t / lambda_c) # [rad]
+    eta2 = np.arctanh(lambda_s / t_)
+
+    # (6) x, yの計算
+    x = A_ * (xi2 + np.sum(np.multiply(alpha_array[1:],
+                                       np.multiply(np.sin(2*xi2*np.arange(1,6)),
+                                                   np.cosh(2*eta2*np.arange(1,6)))))) - S_ # [m]
+    y = A_ * (eta2 + np.sum(np.multiply(alpha_array[1:],
+                                        np.multiply(np.cos(2*xi2*np.arange(1,6)),
+                                                    np.sinh(2*eta2*np.arange(1,6)))))) # [m]
+    # return
+    return x, y # [m]
+
 
 if __name__ == "__main__":
 
-    steps = [2,63,6,3,9,43,74,7]
-    calc_latlon(45, 135,120,steps[0])
+    #steps = [2,63,6,3,9,43,74,7]
+    #calc_moving(45, 135,120,steps[0])
+    START = {'longitude': -157.85059440538518, 'latitude': 21.292003771901275}
+    GOAL = {'longitude': -157.84389066977633, 'latitude': 21.289743961838436}
+    print(calc_theta(START, GOAL))
